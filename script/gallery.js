57161201aee09cbc02e26b77334e25cacad8fc3f
hhhyoung <137036313+hhhyoung@users.noreply.github.com>
2023-07-03 16:48:09 +0900

    Add files via upload

--- script/gallery.js
+++ script/gallery.js
@@ -4,7 +4,7 @@
     spaceBetween: 60,//갤러리 사이 간격
     speed: 2000,//버튼을 슬라이드가 넘어가는 시간
 	  autoplay: {
-        delay: 2000,//자동으로 넘어가기 전 머무르는 시간
+        delay: 3000,//자동으로 넘어가기 전 머무르는 시간
         disableOnInteraction: false,
       },
       loop: true,//슬라이드 무한반복
